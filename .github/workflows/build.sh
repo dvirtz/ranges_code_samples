7110ef9b681ecb4a5fa3d724ca32736cc60c653b
Dvir Yitzchaki <dyitzchaki@oath.com>
2019-10-03 10:41:37 +0300

    build debug and replace

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,5 +2,5 @@
 
 conan remote add onebaseroach https://api.bintray.com/conan/y4n9squared/onebaseroach
 conan remote add mputsz https://api.bintray.com/conan/mpusz/conan-mpusz
-cmake -S repo -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DRUN_TESTS_POSTBUILD=OFF
+cmake -S repo -B build -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DRUN_TESTS_POSTBUILD=ON
 cmake --build ./build
