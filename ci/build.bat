d567720e4c78d08c9c7331bf162ba7231bb26bef
dvirtz <dvirtz@gmail.com>
2019-11-08 01:13:05 +0200

    update readme and build scripts

--- ci/build.bat
+++ ci/build.bat
@@ -1,4 +1,5 @@
 conan remote add onebaseroach https://api.bintray.com/conan/y4n9squared/onebaseroach
 conan remote add mputsz https://api.bintray.com/conan/mpusz/conan-mpusz
+conan remote add dvirtz https://api.bintray.com/conan/dvirtz/conan
 cmake -S %SOURCE_FOLDER% -B build -G Ninja -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DRUN_TESTS_POSTBUILD=ON
 cmake --build ./build
